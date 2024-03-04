7a8662b7a6b7b1b6c447b2e076ced7b18c250d0e
George Camac <60245132+DarkPoisonxx@users.noreply.github.com>
2024-03-04 13:04:25 -0500

    added hi

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello class my name is George this is my repo");
+        System.out.println("boo");
+    }
+    public static void hi(){
+        
     }
 }
